6c8e27e611984319959dc73a2ff705d4445450db
muammerOnerclear <muammer10er@gmail.com>
2024-02-04 19:15:24 +0100

    save

--- src/test/java/junit/utilities/GitTest.java
+++ src/test/java/junit/utilities/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println(" Git branch test");
         System.out.println(" Perkect Test");
-    }
+        System.out.println("Already working");
+       }
 }
