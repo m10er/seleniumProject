57510020e53dc9a0ade8a9be0b1e73a78658a571
muammerOnerclear <muammer10er@gmail.com>
2024-02-04 18:15:58 +0100

    version1

--- src/test/java/junit/utilities/GitBranchTest.java
+++
@@ -1,7 +0,0 @@
-package junit.utilities;
-
-public class GitBranchTest {
-    public static void main(String[] args) {
-        System.out.println( " it s a try to change branch");
-    }
-}
---
+++ src/test/java/junit/utilities/GitTest.java
@@ -0,0 +1,7 @@
+package junit.utilities;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(" Git branch test");
+    }
+}
